c62c6a451fb2608ba02f1cf546424108f5d359d9
8PrakashPrasad <prakashprasad_20616@aitpune.edu.in>
2020-12-24 12:42:21 +0530

    Hello World added

---
+++ main.py
@@ -0,0 +1,5 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 18 2019, 23:46:00) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
+>>> 
